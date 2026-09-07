055087c741dbaaf78740ce8c2bf8f399c8a1e469
W1sCont <tmrostik@gmail.com>
2026-09-07 23:04:00 +0200

    task 1

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const div = document.getElementById("main");
+const divPosts = document.getElementById("posts");
 const button = document.getElementById("button");
 const tbody = document.getElementById("tbody");
 
@@ -24,7 +25,6 @@ function addAuthors() {
     const url = "https://jsonplaceholder.typicode.com/users";
     JSONrequest(url)
         .then(function (data) {
-            console.log(data);
             let authorName = "";
 
             for (let i = 0; i < data.length; i += 3) {
@@ -43,19 +43,19 @@ function addAuthors() {
 
 function info(e) {
     let target = e.target;
-    console.log(target.textContent);
     const url = "https://jsonplaceholder.typicode.com/users";
+
     JSONrequest(url)
         .then(function (data) {
             let user = null;
-            data.map(el =>{
-                if(target.textContent === el.name){
+            data.map(el => {
+                if (target.textContent === el.name) {
                     user = el;
                     let buttonShowInfo = document.createElement("button");
                     buttonShowInfo.id = "buttonShowInfo";
                     buttonShowInfo.textContent = "Show posts";
                     buttonShowInfo.classList = "action-btn";
-
+                    let userId = el.id;
                     let name = "<tr><th>Name:</th>" + "<th>" + user.name + "</th>";
                     let username = "<tr><th>Username:</th>" + "<th>" + user.username + "</th>";
                     let address = "<tr><th>Name:</th>" + "<th>" + user.address.city + ", " + user.addressstreet + "</th>";
@@ -66,6 +66,35 @@ function info(e) {
                     let infoTable = "<h3>User info:</h3>" + "<table>" + `${name}${username}${address}${email}${phone}${website}` + "</table>";
                     div.innerHTML = infoTable;
                     div.appendChild(buttonShowInfo);
+
+                    divPosts.innerHTML = "";
+
+                    buttonShowInfo.addEventListener("click", () => {
+                        const url = "https://jsonplaceholder.typicode.com/posts?";
+                        const params = `userId=${userId}`;
+                        JSONrequest(url + params)
+                            .then(function (data) {
+                                let userPosts = "";
+
+                                for (let i = 0; i < data.length; i += 2) {
+                                    let content = "";
+                                    for (let j = i; j < i + 2 & j < data.length; j++) {
+                                        content += 
+                                            `<th>
+                                            <h3>
+                                            ${data[j].title}
+                                            </h3>
+                                            ${data[j].body}
+                                            </th>`;
+                                    }
+                                    userPosts += `<tr>${content}</tr>`;
+                                }
+                                divPosts.innerHTML = "<h3>User's posts:</h3>" + "<table>" + `${userPosts}` + "</table>";
+                            })
+                            .catch(function (error) {
+                                console.error("Error: " + error);
+                            });
+                    });
                 }
             });
         })
@@ -74,7 +103,5 @@ function info(e) {
         });
 }
 
-
 document.addEventListener("DOMContentLoaded", addAuthors);
 tbody.addEventListener("click", info);
-
